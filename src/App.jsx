1a5f394bc6ee33eaaa6b598b1d0e91a5d9b057ca
FalcaoCamilla <millafalcao.acad@gmail.com>
2023-04-19 11:54:09 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Banner from './components/Banner/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner></Banner>
+      
+    </div>
+  );
+}
+
+export default App;
